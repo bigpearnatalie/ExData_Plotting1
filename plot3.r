d5d560e365a00b5d1b6565c3a8d5936de38ff224
bigpearnatalie <natalieshang@hotmail.com>
2019-08-06 20:13:40 +0800

    Add files via upload

--- plot3.r
+++ plot3.r
@@ -1,5 +1,5 @@
-sdata<-read.table("./household_power_consumption.txt",sep=";",stringsAsFactors=FALSE)
-fdata<-subset(sdata,sdata$V1=="2/1/2007"|sdata$V1=="2/2/2007")
+sdata<-read.table("./household_power_consumption.txt",sep=";", stringsAsFactors=FALSE)
+fdata<-subset(sdata,sdata$V1=="1/2/2007"|sdata$V1=="2/2/2007")
 names(fdata)<-c("Date",	"Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
 fdate<-as.character(fdata$Date)
